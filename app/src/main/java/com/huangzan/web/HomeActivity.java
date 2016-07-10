2c1a6a460af75a53d1aff7e51d29df7c830d09e8
huangshouzan <huangzan2015@gmail.com>
2016-07-10 09:16:19 +0800

    浏览器app-2:顶部栏添加搜索功能,默认使用百度搜索

--- app/src/main/java/com/huangzan/web/HomeActivity.java
+++ app/src/main/java/com/huangzan/web/HomeActivity.java
@@ -1,33 +1,106 @@
 package com.huangzan.web;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 
 public class HomeActivity extends AppCompatActivity {
     private WebView webview;
     private WebSettings webSettings;
     private WebViewClient client;
+    private static boolean isExit = false;
+    private static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            isExit = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         webview = (WebView) findViewById(R.id.wb_home);
-        webSettings = webview.getSettings();
+        if (webview != null) {
+            webSettings = webview.getSettings();
+        }
         client = new OwnerWebview();
         webSettings.setDefaultTextEncodingName("utf-8");
         webview.setWebViewClient(client);
         webview.loadUrl("http://www.baidu.com");
         webSettings.setJavaScriptEnabled(true);
     }
-    private  class  OwnerWebview extends WebViewClient{
+
+    private class OwnerWebview extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        return true;
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (query.startsWith("http://")) {
+                webview.loadUrl(query);
+            } else {
+                webview.loadUrl("http://www.baidu.com/baidu?word=" + query);
+
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            if (!isExit) {
+                isExit = true;
+                Toast.makeText(getApplicationContext(), "再按一次退出", Toast.LENGTH_SHORT).show();
+                handler.sendEmptyMessageDelayed(0, 2000);
+            } else {
+                finish();
+                System.exit(0);
+            }
+        }
+    }
+
+
 }
